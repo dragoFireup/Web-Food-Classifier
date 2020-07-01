313427a62ef9758efc40c289c460811ec7913dee
R Vrishab Jain <jainvrishab@gmail.com>
2020-07-02 01:27:55 +0530

    Added File Submission and stored in static folder

--- app.py
+++ app.py
@@ -1,15 +1,49 @@
 from fastai import *
 from fastai.vision import *
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect
+
+from werkzeug.utils import secure_filename
+import os
+
+
+UPLOAD_FOLDER = os.getcwd()+"\\static"
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
 
 app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config["SECRET_KEY"] = "some_key"
 
-@app.route('/')
+def allowed_file(filename):
+	return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/", methods = ["GET", "POST"])
 def home():
-	return render_template('index.html')
+	
+	if request.method == "GET":
+		return render_template("index.html")
+
+	elif request.method == "POST":
+
+		print(request.files)
+
+		if "file" not in request.files:
+			flash("No File Part")
+			return redirect(request.url)
+
+		file = request.files["file"]
+		print(file)
+
+		if file.filename == "":
+			flash("No Selected file")
+			return redirect(request.url)
+
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+			return redirect(request.url)
 
 
 
 if __name__ == "__main__":
-	app.run('localhost', port=80, debug=True)
+	app.run("localhost", port=80, debug=True)
